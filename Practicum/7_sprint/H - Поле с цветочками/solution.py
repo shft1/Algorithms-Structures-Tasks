4875f1500c225b7e4707526a63ae0089f46a2385
alex <alexmal24122004@gmail.com>
2025-09-10 13:27:37 +0300

    H - Поле с цветочками

---
+++ Practicum/7_sprint/H - Поле с цветочками/solution.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def solution(n, m, grid):
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            dp[i][j] = int(grid[i - 1][j - 1]) + max(
+                dp[i - 1][j], dp[i][j - 1]
+            )
+    return dp[n][m]
+
+
+def main():
+    n, m = map(int, sys.stdin.readline().rstrip().split())
+    grid = [sys.stdin.readline().rstrip() for _ in range(n)][::-1]
+    sys.stdout.write(str(solution(n, m, grid)))
+
+
+if __name__ == "__main__":
+    main()
