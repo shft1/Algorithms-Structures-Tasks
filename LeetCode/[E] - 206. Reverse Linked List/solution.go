848b2b3ad5acfd5fd5ea78d63c446bffeb89f3d6
alex <alexmal24122004@gmail.com>
2025-11-25 13:29:35 +0300

    206. Reverse Linked List

---
+++ LeetCode/[E] - 206. Reverse Linked List/solution.go
@@ -0,0 +1,18 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	var next *ListNode
+	for head != nil {
+		next = head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+	return prev
+}
