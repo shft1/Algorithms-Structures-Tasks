b3346859b763ff976ed42c6c743223c025fec4f2
alex <alexmal24122004@gmail.com>
2025-12-03 19:21:43 +0300

    2390. Removing Stars From a String

---
+++ LeetCode/[M] - 2390. Removing Stars From a String/solution.go
@@ -0,0 +1,13 @@
+package main
+
+func removeStars(s string) string {
+	stack := make([]rune, 0, len(s))
+	for _, char := range s {
+		if char == '*' {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, char)
+		}
+	}
+	return string(stack)
+}
